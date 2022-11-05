61acf2194825747ab60cd338c6cfef2dbdc66ce5
Fatihozn <100231583+Fatihozn@users.noreply.github.com>
2022-11-05 19:41:16 +0300

    Search bar is aktive

--- src/App.js
+++ src/App.js
@@ -7,13 +7,30 @@ import {
   TextInput,
   View,
 } from "react-native";
-import data from "./data.json";
+import Data from "./data.json";
 import Card from "./components/Card";
 import style from "./style";
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [input, setInput] = useState("");
+  const [data, setData] = useState(Data);
   const renderFunction = ({ item }) => <Card data={item} />;
   const keyFunction = (item) => item.id.toString();
+
+  useEffect(() => {
+    setData(Data);
+    if (input !== "") {
+      const filtred = Data.filter(
+        (item) => item.title.toLowerCase().indexOf(input.toLowerCase()) !== -1
+      );
+      setData(filtred);
+    }
+  }, [input]);
+
+  const onChange = (event) => {
+    setInput(event.nativeEvent.text);
+  };
   return (
     <SafeAreaView style={style.container}>
       <Text
@@ -26,7 +43,7 @@ export default function App() {
       >
         My Store
       </Text>
-      <TextInput style={style.text} placeholder="Ara.." />
+      <TextInput onChange={onChange} style={style.text} placeholder="Ara.." />
       <FlatList
         showsVerticalScrollIndicator={false}
         keyExtractor={keyFunction}
